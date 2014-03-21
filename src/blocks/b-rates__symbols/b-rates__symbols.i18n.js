2c2486121d24bd7aed19331363364a61a6a34c07
norlin <myxaxaxa@gmail.com>
2014-03-21 21:59:32 +0400

    Начало выбора нужных пар

--- src/blocks/b-rates__symbols/b-rates__symbols.i18n.js
+++ src/blocks/b-rates__symbols/b-rates__symbols.i18n.js
@@ -1,5 +1,5 @@
 (function (i18n) {
 	i18n['b-rates__symbols'] = {
-
+		add: 'Добавить'
 	};
 } (window.i18n));
--- src/blocks/b-rates__symbols/b-rates__symbols.js
+++ src/blocks/b-rates__symbols/b-rates__symbols.js
@@ -1,7 +1,25 @@
 (function (window, $) {
 	$.register('b-rates__symbols', {
 		init: function () {
-			this.render();
+			var favorites = [
+					'EUR',
+					'RUB'
+				];
+
+			this.listen('updateRates', (data) => {
+				var results = {favorites:[]};
+
+				results.all = data.rates.filter(function (rate) {
+					if (favorites.indexOf(rate.id) > -1) {
+						results.favorites.push(rate);
+						return false;
+					}
+
+					return true;
+				});
+
+				this.render(undefined, results);
+			});
 		}
 	});
 }(window, window.$));
